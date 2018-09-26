39aa370c33cad38afac9cad9e6956a821bf85865
Juan Carlos Sandoval <sandoval1302@build.tamu.edu>
2018-09-25 22:36:45 -0500

    Initial commit

---
+++ Assets/PixelateImageEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelateImageEffect : MonoBehaviour {
+    public Material effectMaterial;
+	// Use this for initialization
+    [ExecuteInEditMode]
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture  dst)
+    {
+        Graphics.Blit(src, dst, effectMaterial);
+    }
+}
